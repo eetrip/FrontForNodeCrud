15874b40b422a0b60eb48070aeef445211ad7b80
eetrip <eeshantripathi111@gmail.com>
2020-03-05 19:52:45 +0530

    changes

---
+++ src/Components/Error.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../index.css";
+
+class ERROR extends Component {
+  render() {
+    return <h1>ERROR 404 not found</h1>;
+  }
+}
+
+export default ERROR;
---
+++ src/Components/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+// Our all component files
+import ListNote from "../Components/ListNote";
+import AddNote from "../Components/AddNote";
+import EditNote from "../Components/EditNote";
+import ERROR from "../Components/Error";
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path="/" component={ListNote} />
+          <Route path="/notes" component={ListNote} />
+          <Route path="/notes/add" component={AddNote} />
+          <Route path="/notes/:id" component={EditNote} />
+          <Route path="/*" component={ERROR} />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Main;
